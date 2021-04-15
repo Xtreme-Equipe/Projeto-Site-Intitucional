c6a3fd6e11930ccab3b114019fd85b1a53ed826a
Elen <petrielen@outlook.com>
2021-04-14 22:11:28 -0300

    Criado botões para editar a página home e descartar alterações feitas na mesma

--- Administrador/admin_header.php
+++ Administrador/admin_header.php
@@ -12,12 +12,34 @@
     if(!$islogado){
         echo '<!--';
     }
+    $admin = isset($_GET['admin']) ? $_GET['admin'] : "0";
+    $class_editar = '';
+    $class_descartar = 'hidden';
+
+    if($admin=='1'){
+        $class_editar = 'hidden';
+        $class_descartar = '';
+    }
 ?>
+
 <div class="barra_admin btn">
-    <div class='welcome'>
-        Bem-vindo(a) <strong><?=$nome_logado?></strong>
-    </div>
     <div>
+        <form class="<?=$class_editar?>" action="" method="GET"><!--habilita a edição da página-->
+            <input type="hidden" name="admin" value="1">
+            <button class="editar">Editar página</button>
+        </form>
+
+        <form class="<?=$class_descartar?>" action="" method="GET"> <!--habilita o descarte de alterações-->
+            <input type="hidden" name="admin" value="0">
+            <button class="descartar">Descartar</button>
+        </form>
+
+    </div>
+    <span class="welcome">
+        Bem-vindo(a) <strong><?=$nome_logado?></strong>
+</span>
+    
+   <div class="sair">
         <form action="../Administrador/login.php" method="POST">
             <input type="hidden" name="logout" value="True">
             <button class="sair">Sair</button>
