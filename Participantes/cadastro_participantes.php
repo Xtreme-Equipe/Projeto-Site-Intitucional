a50219e85356709b5afdbd3cd79da9a02f5ecda9
Elen <petrielen@outlook.com>
2021-05-14 07:50:51 -0300

    Corrigido o linkque redireciona para cadastro concluído

--- Participantes/cadastro_participantes.php
+++ Participantes/cadastro_participantes.php
@@ -117,7 +117,7 @@ if ($texto != "") {
             </div><!--center-conteúdo-->
         </section><!--conteúdo-->
 
-        <form class="formulario" method = "post" action="cadastrado_com_sucesso2.php">
+        <form class="formulario" method = "post" action="participante_concluido.php">
             <div class="center-form">
 
                 <div class="titulo-form">
