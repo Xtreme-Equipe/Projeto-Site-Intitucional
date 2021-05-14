ff677a451d9d2b85553c9b4d9af88a7f4f252041
Elen <petrielen@outlook.com>
2021-05-14 09:29:51 -0300

    Inclusão do icon na página de login

--- Administrador/login.php
+++ Administrador/login.php
@@ -47,6 +47,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./login_admin.css">
+    <link rel="icon" href="../Home/imagem/Vó-logo.png">
 
     <title>Controle de acesso</title>
 
